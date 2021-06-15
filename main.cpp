715d406e39f93ead62a320797f1098e32ea56537
Yash Dalmia <yashdalmia@Yashs-MacBook-Air.local>
2021-06-14 22:01:33 -0700

    finish chess board & move setup

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
-#include "utils/exception.h"
+#include "chess/board.h"
+
+/* NOTE: we have copied only the chess board & move representation code from lczero
+ * into this project as a static library, compiled with cmake. 
+ * this is not how lczero compiled the code, so there are some rough edges. 
+ */
 
 int main() {
-    std::cout << "in main" << std::endl;
-    throw lczero::Exception("hi");
+    lczero::InitializeMagicBitboards(); //trippy! need to run this before anything else
+    lczero::ChessBoard board;
+    board.SetFromFen("Q3b3/2P2pnk/3R3p/p7/1pp1p3/PnP1P2P/2B2PP1/5RK1 w - - 1 31");
+
+    std::cout << board.DebugString() << std::endl;
+    auto moves = board.GenerateLegalMoves();
+    board.ApplyMove(moves[0]);
+    std::cout << board.DebugString() << std::endl;
+
+    // for (auto move: moves) {
+    //     std::cout << move.as_string() << std::endl;
+    // }
+
     return 0;
 }
